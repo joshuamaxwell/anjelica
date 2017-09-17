5d249f0ac6ac73d07dcd9b0f12c44c243f491cc9
JMaxwell <maxwell.joshua@gmail.com>
2017-09-16 20:46:43 -0400

    push dist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdSidenavModule, MdToolbarModule, MdIconModule, MdButtonModule } from '@angular/material'
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdSidenavModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
